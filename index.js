c66b0bb8d8f61e8f77004fc99fbec96e15de83bf
rajgiri-gauswami <gauswamirajgiri999@gmail.com>
2025-07-06 13:20:58 +0530

    [Add] :-  First Added index.js file

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express()
+const path = require('path');
+const { PORT } = require('./src/config/envconfig');
+const router = require('./src/routes/baseRoute');
+
+// JSON body data access middleware
+app.use(express.json());
+
+//Public access files
+app.use('/assets', express.static(path.join((__dirname, 'src/public'))));
+
+//Routes
+app.get('/', (req, res) => {
+    res.send("<h1 style='color: blue'>Hello Raj...</h1>")
+})
+app.use('/', router);
+
+
+app.listen(PORT, () => {
+    console.log(`Server running of http:localhost:${PORT}`)
+})
